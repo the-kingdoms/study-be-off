199a1c03e1d1a2a2d8334c2303d8beb7a6161e55
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2023-07-23 12:55:21 +0900

    init

---
+++ src/main/java/com/kingdoms/thekingdonsproject/ThekingdonsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kingdoms.thekingdonsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThekingdonsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThekingdonsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kingdoms/thekingdonsproject/ThekingdonsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingdoms.thekingdonsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThekingdonsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
